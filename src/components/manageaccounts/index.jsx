9141d441d4527d9f73ab410ecaf9f44c6bdd3d12
udaypydi <udaypydi333@gmail.com>
2020-10-27 23:39:45 +0530

    feat(manageaccounts): add modal to manage accounts

--- src/components/manageaccounts/index.jsx
+++ src/components/manageaccounts/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { Paragraph } from 'src/commons/text';
 import { FaPlus } from 'react-icons/fa';
 import { BasicCard } from 'src/commons/card';
+import Modal from 'src/commons/modal';
 
 const Container = styled.div`
     display: flex;
@@ -16,36 +17,47 @@ const Container = styled.div`
 
 
 function ManageAccount(props) {
+  const [showAccountsModal, setShowAccountsModal] = useState(false);
+
   return (
-    <Container
-      justify="flex-start"
-      align="flex-start"
-      height="100%"
-      flexDirection="row"
-    >
-       <div
-        className="text-center"
-        id="custom_card"
-      >
-        <BasicCard
-          width="150px"
-          height="150px"
+    <React.Fragment>
+        <Container
+          justify="flex-start"
+          align="flex-start"
+          height="100%"
+          flexDirection="row"
         >
-          <FaPlus
-            size="3em"
-            style={{
-              fontWeight: 100,
-            }}
-          />
-        </BasicCard>
-        <Paragraph
-          weight={600}
-          fontSize="1rem"
-        >
-          Add Account
-        </Paragraph>
-      </div>
-    </Container>
+          <div
+            className="text-center"
+            id="custom_card"
+            onClick={() => setShowAccountsModal(true)}
+          >
+            <BasicCard
+              width="150px"
+              height="150px"
+            >
+              <FaPlus
+                size="3em"
+                style={{
+                  fontWeight: 100,
+                }}
+              />
+            </BasicCard>
+            <Paragraph
+              weight={600}
+              fontSize="1rem"
+            >
+              Add Account
+            </Paragraph>
+          </div>
+        </Container>
+        <Modal
+          title="Add accounts"
+          showModal={showAccountsModal}
+          onModalToggle={setShowAccountsModal}
+          showActionButtons={false}
+        />
+    </React.Fragment>
   );
 }
 
