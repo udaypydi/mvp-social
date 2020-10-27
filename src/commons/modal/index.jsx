92c48ee29a0e42cec352cf67808606d60269b114
udaypydi <udaypydi333@gmail.com>
2020-10-27 23:43:04 +0530

    fix(remove): unused state variables

--- src/commons/modal/index.jsx
+++ src/commons/modal/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from 'prop-types';
+
 
 function Modal({ showModal, title, modalBody, showActionButtons, onModalToggle }) {
   return (
@@ -10,7 +11,7 @@ function Modal({ showModal, title, modalBody, showActionButtons, onModalToggle }
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             onClick={() => onModalToggle(false)}
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-auto my-6 mx-auto max-w-6xl">
            
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
         
