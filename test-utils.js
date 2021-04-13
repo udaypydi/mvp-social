28b7e36417bb5d1458fa8897fe12960b4682bb41
Uday Bhaskhar <udaypydi333@gmail.com>
2021-04-13 13:32:36 +0530

    Update test-utils.js

--- test-utils.js
+++ test-utils.js
@@ -1,6 +1,9 @@
 // test-utils.js
 import { render, queries } from '@testing-library/react'
 
+
+// react testing library
+
 const customRender = (ui, options) =>
   render(ui, { queries: { ...queries }, ...options })
 
